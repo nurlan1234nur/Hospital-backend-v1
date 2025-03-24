c1dce4b978221ff8b01064f1fa067f44f5eaceb4
remote_dev <>
2025-03-25 00:24:33 +0800

    [fix]: delete for now

--- domain/models/MedicalStaff.model.js
+++ domain/models/MedicalStaff.model.js
@@ -5,7 +5,6 @@ import User from "./User.model.js";
 const MedicalStaffSchema = new Schema({
   position: { type: String, enum: ["Doctor", "Nurse"] },
   specialization: { type: String },
-  patients: [{ type: Schema.Types.ObjectId, ref: "Patient" }],
 });
 
 //Mongoose's discriminator feature allows you to create specialized models that inherit from a base model.
