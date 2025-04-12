30c756cd6e539b6504ecd0a25ff19c26df091c17
remote_dev <tsetsuukhei@gmail.com>
2025-04-12 17:49:57 +0800

    [update] added examination FK

--- domain/models/Prescription.model.js
+++ domain/models/Prescription.model.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const PrescriptionSchema = new Schema({
-  date: { type: Date, default: Date.now },
-  prescribedBy: { type: Schema.Types.ObjectId, ref: "MedicalStaff", required: true },
-  patient: { type: Schema.Types.ObjectId, ref: "Patient", required: true }
-}, { timestamps: true });
+const PrescriptionSchema = new Schema(
+  {
+    date: { type: Date, default: Date.now },
+    prescribedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "MedicalStaff",
+      required: true,
+    },
+    patient: { type: Schema.Types.ObjectId, ref: "Patient", required: true },
+    examination_id: { type: Schema.Types.ObjectId, ref: "Examination" },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Prescription", PrescriptionSchema);
