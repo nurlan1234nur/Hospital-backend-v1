a21547d3242e47b7436dcb561681767ff1619bb1
remote_dev <tsetsuukhei@gmail.com>
2025-04-12 17:49:23 +0800

    [reformat]

--- domain/models/PrescribedGuide.model.js
+++ domain/models/PrescribedGuide.model.js
@@ -1,14 +1,25 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const PrescribedGuideSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  dose: { type: String },
-  frequency: { type: String },
-  patient: { type: Schema.Types.ObjectId, ref: "Patient", required: true },
-  medicalStaff: { type: Schema.Types.ObjectId, ref: "MedicalStaff", required: true },
-  prescription: { type: Schema.Types.ObjectId, ref: "Prescription", required: true }
-}, { timestamps: true });
+const PrescribedGuideSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    dose: { type: String },
+    frequency: { type: String },
+    patient: { type: Schema.Types.ObjectId, ref: "Patient", required: true },
+    medicalStaff: {
+      type: Schema.Types.ObjectId,
+      ref: "MedicalStaff",
+      required: true,
+    },
+    prescription: {
+      type: Schema.Types.ObjectId,
+      ref: "Prescription",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("PrescribedGuide", PrescribedGuideSchema);
