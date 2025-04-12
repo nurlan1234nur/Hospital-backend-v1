36764ef4838e333739501ce748fef13fdab0672f
remote_dev <tsetsuukhei@gmail.com>
2025-04-12 17:50:23 +0800

    [reformat]: changed naming

--- domain/models/Treatment.model.js
+++ domain/models/Treatment.model.js
@@ -1,17 +1,21 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const TreatmentSchema = new Schema(
   {
-    date: {type:Date},
-    treatmentType: {type:String, enum:["Uvch","Bumba","Sharlaga","Dusal","Taria"]},
-    diagnosisType: { type: String, enum:["in", "out"] },
-    howManyTimes: {type:Number},
-    howManyDone: {type:Number},
-    patient: { type: Schema.Types.ObjectId, ref: 'Patient' },
+    date: { type: Date },
+    treatmentType: {
+      type: String,
+      enum: ["Uvch", "Bumba", "Sharlaga", "Dusal", "Taria"],
+    },
+    diagnosisType: { type: String, enum: ["in", "out"] },
+    totalQuantity: { type: Number },
+    sessionsCompleted: { type: Number },
+    examination_id: { type: Schema.Types.ObjectId, ref: "Examination" },
+    patient: { type: Schema.Types.ObjectId, ref: "Patient" },
     medicalStaff: { type: Schema.Types.ObjectId, ref: "MedicalStaff" },
   },
   { timestamps: true }
 );
 
-const Treatment = model('Treatment', TreatmentSchema);
+const Treatment = model("Treatment", TreatmentSchema);
 export default Treatment;
