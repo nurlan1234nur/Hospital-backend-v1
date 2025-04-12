d3ac09c8656dc36a9cf29d2c68ba73ca1db9150e
remote_dev <tsetsuukhei@gmail.com>
2025-04-12 17:48:46 +0800

    [update] added examination FK

--- domain/models/MedicalTest.model.js
+++ domain/models/MedicalTest.model.js
@@ -1,13 +1,16 @@
-
 import { Schema } from "mongoose";
 
 const medicalTestSchema = new Schema({
-  testType:{type: String, enum:["blood", "urine"]},
-  dateOfTest: {type:Date},
-  Result:{type:String},
-  ResultStatus:{type:String, enum:["pending","completed","abnormal","normal"]},
-  note:{type:String},
-  patients: [{ type: Schema.Types.ObjectId, ref: "Patient" }],
+  testType: { type: String, enum: ["blood", "urine"] },
+  dateOfTest: { type: Date },
+  Result: { type: String },
+  ResultStatus: {
+    type: String,
+    enum: ["pending", "completed", "abnormal", "normal"],
+  },
+  note: { type: String },
+  examination_id: { type: Schema.Types.ObjectId, ref: "Examination" },
+  patients: { type: Schema.Types.ObjectId, ref: "Patient" },
 });
 
 //Mongoose's discriminator feature allows you to create specialized models that inherit from a base model.
